d0db27a7067affe10a8e9b6f3ad177caa434d161
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-27 18:50:51 -0400

    Populate display on number button click

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const subtract = (x, y) => x - y; // function to subtract two numbers
 const multiply = (x, y) => x * y; // function to multiply two numbers
 const divide = (x, y) => x / y; // function to divide two numbers
 
+enableButtons();
+
 // operate function; input: two numbers, callback: operation to perform on input numbers
 function operate (operator, x, y) {
   // set cb to one of above callbacks based on operator
@@ -28,9 +30,30 @@ function operate (operator, x, y) {
   return cb(x, y);
 }
 
-// function that enables buttons to populate display
-  // select display
-  // select buttons
-  // obj: keys for each button text, values for desired result (number or function)
+// obj: keys for each button text, values for desired result (number or function)
 
-  // for each button
+// function that enables buttons to populate display
+function enableButtons() { 
+  const numbers = document.querySelectorAll('.number'); // select all number buttons
+  // for each number button, add event listener: click, populateDisplay
+  numbers.forEach(numberBtn => populateDisplay(numberBtn));
+  
+  // select all operation buttons
+}
+
+function populateDisplay(button) {
+  const displayContent = document.querySelector('#display-content');
+  button.addEventListener('click', () => {
+    if (displayContent.innerText === '0') {
+      // replace displayContent text with button text
+      displayContent.innerText = button.innerText;
+      // store button text in firstNum
+      firstNum = button.innerText;
+    } else if (displayContent.innerText.length < 12) {
+      // append button text to displayContent text
+      displayContent.innerText += button.innerText;
+      // append button text to firstNum
+      firstNum += button.innerText;
+    }
+  });
+}
