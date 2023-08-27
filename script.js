8c3cae53981d46f140c85f0c421fae65ad6dac46
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-27 12:01:49 -0400

    Write basic logic for calculator operations

---
+++ script.js
@@ -0,0 +1,29 @@
+let firstNum;
+let operator;
+let secondNum;
+
+const add = (x, y) => x + y; // function to add two numbers
+const subtract = (x, y) => x - y; // function to subtract two numbers
+const multiply = (x, y) => x * y; // function to multiply two numbers
+const divide = (x, y) => x / y; // function to divide two numbers
+
+// operate function; input: two numbers, callback: operation to perform on input numbers
+function operate (operator, x, y) {
+  // set cb to one of above callbacks based on operator
+  let cb;
+  switch(operator) {
+    case '+':
+      cb = add;
+      break;
+    case '-':
+      cb = subtract;
+      break;
+    case '*':
+      cb = multiply;
+      break;
+    case '/':
+      cb = divide;
+      break;
+  }
+  return cb(x, y);
+}
