0640b5e9272c285d88c46dbf20d05dade793cca9
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-29 12:02:35 -0400

    Make basic functions of calculator work

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let firstNum;
+let firstNum = 0;
 let operator;
 let secondNum;
+let replaceNum = true;
 
 const add = (x, y) => x + y; // function to add two numbers
 const subtract = (x, y) => x - y; // function to subtract two numbers
@@ -20,10 +21,10 @@ function operate (operator, x, y) {
     case '-':
       cb = subtract;
       break;
-    case '*':
+    case '×':
       cb = multiply;
       break;
-    case '/':
+    case '÷':
       cb = divide;
       break;
   }
@@ -33,27 +34,52 @@ function operate (operator, x, y) {
 // obj: keys for each button text, values for desired result (number or function)
 
 // function that enables buttons to populate display
-function enableButtons() { 
-  const numbers = document.querySelectorAll('.number'); // select all number buttons
-  // for each number button, add event listener: click, populateDisplay
-  numbers.forEach(numberBtn => populateDisplay(numberBtn));
+function enableButtons() {
+  const displayContent = document.querySelector('#display-content');
+
+  // for each number button, pass into function to add event listener
+  const numbers = document.querySelectorAll('.number');
+  numbers.forEach(numberBtn => populateDisplay(numberBtn, displayContent));
   
-  // select all operation buttons
+  // for each operation button, pass into function to add event listener
+  const operations = document.querySelectorAll('.operation');
+  operations.forEach(operationBtn => enableOperations(operationBtn, displayContent));
+
+  // pass equals button into function to add event listener
+  const equals = document.querySelector('.equals');
+  enableEquals(equals, displayContent);
 }
 
-function populateDisplay(button) {
-  const displayContent = document.querySelector('#display-content');
+// function to add event listeners to number buttons to populate the display
+function populateDisplay(button, displayContent) {
   button.addEventListener('click', () => {
-    if (displayContent.innerText === '0') {
+    if (replaceNum) {
       // replace displayContent text with button text
       displayContent.innerText = button.innerText;
-      // store button text in firstNum
-      firstNum = button.innerText;
+      replaceNum = false;
     } else if (displayContent.innerText.length < 12) {
       // append button text to displayContent text
       displayContent.innerText += button.innerText;
-      // append button text to firstNum
-      firstNum += button.innerText;
     }
+  });
+}
+
+// function to add event listeners to operation buttons to: set operator, firstNum, allow new num to be entered
+function enableOperations(button, displayContent) {
+  button.addEventListener('click', () => { // add click event listener to button
+    operator = button.innerText; // set operator to button text
+    firstNum = Number(displayContent.innerText); // set firstNum to displayContent text, converted to Number
+    replaceNum = true;
+  });
+}
+
+function enableEquals(button, displayContent) {
+  button.addEventListener('click', () => { // add click event listener to button
+    secondNum = Number(displayContent.innerText); // set secondNum to displayContent text, converted to Number
+    // pass operator, firstNum, secondNum into operate function; set to res
+    let res = operate(operator, firstNum, secondNum);
+    displayContent.innerText = res; // change displayContent text to res
+    firstNum = res; // change firstNum to res
+    replaceNum = true;
   });
 }
